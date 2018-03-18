bdeaa3d771e5f734b17cd30f140d938633312da3
Woimbée Arthur <arthur.woimbee@gmail.com>
2018-03-18 19:20:01 +0100

    meilleurs déplacements, amélioration lisibilité rotate2D Rétilisation de la fonction rotate2D pour appliquer les rotations Création de plusieurs fonctions : def keydown(event): def keyup(event): def movement():
    Suppression de :
    def keypress(event):
    
    (anciennement version 8.7)

--- moteur3D.py
+++ moteur3D.py
@@ -3,6 +3,7 @@ from PIL import ImageTk, Image
 import math, random
 import time
 import os
+import _thread
 
 WIDTH = 1000
 HEIGHT = 1000
@@ -11,76 +12,72 @@ sensMouv = 1/5 #sensibilite des mouvements
 sensRot = 1/10 #sensibilite de la rotation
 
 colors  = ("#6D8572", "#FAE705", "#343A83", "#00FF00", "#FFB400", "#AD009E")
+pressedkeys=[]
 
 class Camera:
     def __init__(self, pos=(0,0,0), rot=(0,0)):
         self.pos = list(pos)
         self.rot = list(rot) #rot=rotation
 
-def keypress(event):
+def keydown(event):
+    if event.keysym not in pressedkeys:
+        pressedkeys.append(event.keysym)
+
+def keyup(event):
+    if event.keysym in pressedkeys:
+        pressedkeys.pop(pressedkeys.index(event.keysym))
+
+def movement():
     """Calcule le déplacement et l'ordre d'affichage des objets en fonction de l'input."""
-    #deplacement
-    global cos, sin
-    x,y = sin[1]*sensMouv, cos[1]*sensMouv
-    if event.keysym == 'd':
-        cam.pos[0]+=y
-        cam.pos[2]-=x
-    elif event.keysym == 'a':
-        cam.pos[0]-=y
-        cam.pos[2]+=x
-    elif event.keysym == 'w':
-        cam.pos[0]+=x
-        cam.pos[2]+=y
-    elif event.keysym == 's':
-        cam.pos[0]-=x
-        cam.pos[2]-=y
 
-    elif event.keysym == 'q':
-        cam.pos[1]+=sensMouv
-    elif event.keysym == 'e':
-        cam.pos[1]-=sensMouv
+    sensMouv = 1/10 #sensibilite des mouvements
+    sensRot = 1/40 #sensibilite de la rotation
+    # import time as time
+    while(1):
+        time.sleep(0.01)
+        for key in pressedkeys:
+            x,y = math.sin(cam.rot[1]), math.cos(cam.rot[1])
+            if key == 'd':
+                cam.pos[0]+=y
+                cam.pos[2]-=x
+            elif key == 'a':
+                cam.pos[0]-=y
+                cam.pos[2]+=x
+            elif key == 'w':
+                cam.pos[0]+=x
+                cam.pos[2]+=y
+            elif key == 's':
+                cam.pos[0]-=x
+                cam.pos[2]-=y
 
-    #exit
-    elif event.keysym == 'Escape':
-        root.destroy()
+            elif key == 'q':
+                cam.pos[1]+=sensMouv
+            elif key == 'e':
+                cam.pos[1]-=sensMouv
 
-    #rotation
-    # axe X     axe Y
-    # rot[1]    rot[0]
-    elif event.keysym == 'Left':
-        cam.rot[1]-=sensRot
-    elif event.keysym == 'Right':
-        cam.rot[1]+=sensRot
-    elif event.keysym == 'Up':
-        cam.rot[0]-=sensRot
-    elif event.keysym == 'Down':
-        cam.rot[0]+=sensRot
+            #exit
+            elif key == 'Escape':
+                root.destroy()
+
+            #rotation
+            # axe X     axe Y
+            # rot[1]    rot[0]
+            elif key == 'Left':
+                cam.rot[1]-=sensRot
+            elif key == 'Right':
+                cam.rot[1]+=sensRot
+            elif key == 'Up':
+                cam.rot[0]-=sensRot
+            elif key == 'Down':
+                cam.rot[0]+=sensRot
 
 
-    ##########################################################
-    # REVOVE NOT SHOWED OBJECTS
-    #mettre ça ici permet d'améliorer le framerate
-    #on calcule dans quel ordre dessiner chaque cube - *on retire ceux qui sont hors champ*
-    global objects
-    for obj in objects :
-        obj.show = False
-        x,y,z = obj.pos     #tout cela est calculé à partir du centre de chaque cube
-        x-=cam.pos[0]
-        y-=cam.pos[1]
-        z-=cam.pos[2]
+def rotate2D(pos, rotation):
+    #pos[0] represente x ou y en fonction des cas, pos[1]=z
+    sin=math.sin(rotation)
+    cos=math.cos(rotation)
+    return pos[0]*cos-pos[1]*sin, pos[1]*cos+pos[0]*sin
 
-        x,z = x*cos[1]-z*sin[1], z*cos[1]+x*sin[1]   #x et z modifies par la rotation autour de y
-        y,z = y*cos[0]-z*sin[0], z*cos[0]+y*sin[0] #y et z modifies par la rotation autour de x
-
-        if z>0:
-            #si z=0 on a une division par 0 et si z<0 alors l'affichage est hors champ
-            f=SWidth/z #coefficient de stereoscopie 
-            X,Y = int(x*f)+SWidth, int(y*f)+SHeight #position en pixels des sommets sur l'image 2D
-
-            if X<WIDTH+SWidth and Y<HEIGHT+SHeight and X>-SWidth and Y>-SHeight: #peu d'impact sur framerate
-                obj.depth = (x**2)+(y**2)+(z**2)
-                obj.show = True
-    ############################################################# super fast
 
 
 
@@ -122,7 +119,9 @@ frame.grid_columnconfigure(0, weight=1)
 
 canvas = tkinter.Canvas(frame, width=WIDTH, height=HEIGHT, bg="#ffffff")
 canvas.grid(row=0, column=0, sticky=tkinter.N+tkinter.S+tkinter.E+tkinter.W)
-root.bind("<Key>", keypress)
+root.bind("<KeyPress>", keydown)
+root.bind("<KeyRelease>", keyup)
+
 frame.pack()
 
 ##Fond d'ecran
@@ -153,59 +152,65 @@ for x in range(0,20,2):
 ##objects.extend( [Cube((0,0,0),"red"),Cube((2,0,0), "blue"), Cube((-2,0,0), "yellow")])        
 
 i,frameRate=0,0
+_thread.start_new_thread(movement, ( ))
 while True:
     t0 = time.time()
 
-    cos = (math.cos(cam.rot[0]), math.cos(cam.rot[1]))
-    sin = (math.sin(cam.rot[0]), math.sin(cam.rot[1]))
+
+
+    face_list=[] #contient : [points1, points2, ...] => [ [ (x,y),(x,y),(x,y),(x,y), tuple(couleur), int(profondeur) ], [ (x,y),...], ...]
     
-    objects.sort(key=lambda x: x.depth, reverse=True) #on ordonne objects selon l'ordre de depth
-    #on affiche pas les objets les plue eloignes
-    crop = int(len(objects)/10)
     #on calcule comment dessiner chaque cube
-    for obj in objects[crop:] :
-        if obj.show :
-            face_list=[] #contient : [points1, points2, ...] => [ [ (x,y),(x,y),(x,y),(x,y), tuple(couleur), int(profondeur) ], [ (x,y),...], ...]
-            for face in obj.faces:
+    for obj in objects :
+
+        obj_faces=[]
+        for face in obj.faces:
                 
-                depth = 0
-                face_points = [] #contient 4 sommets a connecter -> (x,y),(x,y),(x,y),(x,y)
-                for x,y,z in (obj.vertices[face[0]], obj.vertices[face[1]], obj.vertices[face[2]], obj.vertices[face[3]]):
+            depth = 0
+            face_points = [] #contient 4 sommets a connecter -> (x,y),(x,y),(x,y),(x,y)
+            for x,y,z in (obj.vertices[face[0]], obj.vertices[face[1]], obj.vertices[face[2]], obj.vertices[face[3]]):
                     #le monde bouge par rapport a la camera et non le contraire
-                    x-=cam.pos[0]
-                    y-=cam.pos[1]
-                    z-=cam.pos[2]
+                x-=cam.pos[0]
+                y-=cam.pos[1]
+                z-=cam.pos[2]
 
-                    x,z = x*cos[1]-z*sin[1], z*cos[1]+x*sin[1] #x et z modifies par la rotation autour de y
-                    y,z = y*cos[0]-z*sin[0], z*cos[0]+y*sin[0] #y et z modifies par la rotation autour de x
+                x,z = rotate2D((x,z),cam.rot[1]) #x et z modifies par la rotation autour de y
+                y,z = rotate2D((y,z),cam.rot[0]) #y et z modifies par la rotation autour de x
 
-                    if z>0:
-                        f=SWidth/z #coefficient de stereoscopie 
-                        face_points.append((x*f+SWidth, y*f+SHeight)) #position en pixels des sommets sur l'image 2D
-                        depth += (x**2)+(y**2)+(z**2) #se calcule avec *petit* x,y,z car ils sont position en 3d là où Y,X sont en 2D
-                if len(face_points) > 1:
-                    #si il y a au moins 2 points à connecter
-                    face_points.extend( (obj.color, depth) )
+                if not z>0:
+                    face_points = None
+                    break
+                f=SWidth/z #coefficient de stereoscopie
+                X,Y = int(x*f)+SWidth, int(y*f)+SHeight #position en pixels des sommets sur l'image 2D ; +SWidth et SHeight car le repere xyz est placé au milieu de l'ecran
+
+                if not -50<X<WIDTH+50 or not -50<Y<HEIGHT+50 : #peut etre ajuste pour les performances
+                    #on affiche pas ce qui est hors champ
+                    face_points=None
+                    break
+
+                face_points.append((X, Y)) #position en pixels des sommets sur l'image 2D
+                depth += (x**2)+(y**2)+(z**2) #se calcule avec *petit* x,y,z car ils sont position en 3d là où Y,X sont en 2D
+            if face_points is not None:
+                #si il y a au moins 2 points à connecter
+                face_points.extend( (obj.color, depth) )
                     #face_points[4] est dedié à la couleur, cela permet une meilleure optimisation (20fps au lieu de 10!)
                     #face_point[5] dedié à la profondeur
-                    face_list.append(face_points)
+                
+                obj_faces.append(face_points)
+        obj_faces.sort(key=lambda x: x[-1], reverse=True)     
+        face_list.extend(obj_faces[3:]) #vu qu'on affiche que des cubes il suffit d'afficher les 3 faces visibles et non les 6 faces du cube
         
-            ##DO THE DRAWING HERE
-            face_list.sort(key=lambda x: x[-1], reverse=True)
-            for face in face_list[-3:]: #on ne dessine que les 3 faces maximum visibles simultanément
-                canvas.create_polygon(face[:-2], fill=face[-2], outline="black", tag="face")
+    face_list.sort(key=lambda x: x[-1], reverse=True)
+    for face in face_list: #on ne dessine que les 3 faces maximum visibles simultanément
+        canvas.create_polygon(face[:-2], fill=face[-2], outline="black", tag="face")
 
 
     root.update()
-    #on remet l'image à 0
-
-    ##tLOL = time.time()
-    canvas.delete("face")
-    ##print(( time.time()-tLOL ), "canvas delete")
+    canvas.delete("face") #on remet l'image à 0
 
     frameRate += time.time()-t0
     i+=1
     if i > 50 :
-        print(1/((frameRate/i)+1e-10),"1/fps")
+        print(1/((frameRate/i)+1e-10),"fps")
         i=0
         frameRate=0
