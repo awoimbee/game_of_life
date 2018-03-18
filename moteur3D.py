5d747b32cfbfbd95b7bff877a5923275944721ef
Woimbée Arthur <arthur.woimbee@gmail.com>
2018-03-18 18:58:44 +0100

    moteur3D : ajout du déplacement de la caméra

--- moteur3D.py
+++ moteur3D.py
@@ -1,40 +1,68 @@
 import tkinter
 import PIL
 from PIL import ImageTk, Image, ImageDraw
+import time
+import math
 
 WIDTH = 800
 HEIGHT = 800
 SWidth, SHeight = WIDTH//2, HEIGHT//2 #semi width and semi height
-SENSIBILITE = 1
+sensMouv = 0.25 #sensibilite des mouvements
+sensRot = 1/15 #sensibilite de la rotation
 
-#JE VOULAIS FAIRE UNE CLASSE SAUF QUE TKINTER C'EST DE LA MERDE N'ESTCE PAS ?
+
 
 class Camera:
     def __init__(self, pos=(0,0,0), rot=(0,0)):
         self.pos = list(pos)
         self.rot = list(rot) #rot=rotation
 
-def keypress(event):
-    print("keypress !!")
-    #right left forward backward  up  down
-    # ++X  --X    ++Z     --Z    ++Y  --Y
-    if event.keysym == 'd':
-        cam.pos[0]+=SENSIBILITE
-    elif event.keysym == 'a':
-        cam.pos[0]-=SENSIBILITE
-    elif event.keysym == 'w':
-        cam.pos[2]+=SENSIBILITE
-    elif event.keysym == 's':
-        cam.pos[2]-=SENSIBILITE
-    elif event.keysym == 'q':
-        cam.pos[1]+=SENSIBILITE
-    elif event.keysym == 'e':
-        cam.pos[1]-=SENSIBILITE
+def rotate(pos, rotation):
+    a,z=pos #a est x ou y
+    sin=math.sin(rotation)
+    cos=math.cos(rotation)
+    #return z*cos+a*cos, z*sin-x*sin
+    return a*cos-z*sin, z*cos+a*sin
 
+def keypress(event):
+    #deplacement
+    #right left forward backward  up  down
+    # --X  ++X    ++Z     --Z    ++Y  --Y
+    if event.keysym == 'd':
+        cam.pos[0]-=sensMouv
+    elif event.keysym == 'a':
+        cam.pos[0]+=sensMouv
+    elif event.keysym == 'w':
+        cam.pos[2]+=sensMouv
+    elif event.keysym == 's':
+        cam.pos[2]-=sensMouv
+    elif event.keysym == 'q':
+        cam.pos[1]+=sensMouv
+    elif event.keysym == 'e':
+        cam.pos[1]-=sensMouv
+
+    #exit
     elif event.keysym == 'Escape':
         root.destroy()
 
-        
+    #rotation
+    # axe X     axe Y
+    # rot[1]    rot[0]
+    elif event.keysym == 'Left':
+        cam.rot[1]+=sensRot
+    elif event.keysym == 'Right':
+        cam.rot[1]-=sensRot
+    elif event.keysym == 'Up':
+        cam.rot[0]-=sensRot
+    elif event.keysym == 'Down':
+        cam.rot[0]+=sensRot
+    
+    
+def clamp (val, minval, maxval):
+    "retourne val, compris entre minval et maxval"
+    if val < minval: return minval
+    if val > maxval: return maxval
+    return val
 
 ##Création de la fenetre
 root = tkinter.Tk()
@@ -48,8 +76,7 @@ root.bind("<Key>", keypress)
 frame.pack()
 
 
-# chaque frame sera PILimg
-PILimg = Image.new('RGB', (WIDTH,HEIGHT), (255, 255, 255))
+
 
 
 #sommets du cube selon le repere (x,y,z) puis les arretes du cube
@@ -59,43 +86,46 @@ edges = (0,1),(1,2),(2,3),(3,0), (0,4), (1,5),(2,6),(3,7), (4,5),(5,6),(6,7),(7,
 cam=Camera((0,0,-5))
 i=0
 while True:
+    t0 = time.time()
+    # chaque frame sera PILimg
+    PILimg = Image.new('RGB', (WIDTH,HEIGHT), (255,255,255))
     
-    #framecount
-    i+=1
-    print(i, cam.pos)
+
+    
     #position de ces sommets sur l'image 2D
     verticesPxPos = []
 
-    for x,y,z in vertices:
-        z+=6 #la camera est positionee a z=0, on ne veut pas etre dans le cube
-        f=SWidth/z #un coefficient de putain de 3d machin la de comment ca ressort et tout putain en 3d stereoscopique on en parle bcp de cette merde
-        x,y = int(x*f)+SWidth, int(y*f)+SHeight  #donne la position de x et y en pixels
+    draw = ImageDraw.Draw(PILimg)
+    for edge in edges:
+        points = [] #contient 2 sommets a connecter par arrete
+        for x,y,z in (vertices[edge[0]], vertices[edge[1]]):
+            #le monde bouge par rapport a la camera et non le contraire
+            x-=cam.pos[0]
+            y-=cam.pos[1]
+            z-=cam.pos[2]
 
-        verticesPxPos.append([x,y]) #"position de ces sommets sur l'image 2D"
-        PILimg.putpixel ((x,y), ( 0, 0, 0 )) #on met tout sur l'image
+            x,z = rotate((x,z),cam.rot[1]) #x et z modifies par la rotation autour de y
+            y,z = rotate((y,z),cam.rot[0]) #y et z modifies par la rotation autour de x
+            
+            f=SWidth/(z+0.0001) #un coefficient de stereoscopie,    
 
+            x,y = int(x*f)+SWidth, int(y*f)+SHeight #position des sommets sur l'image 2D
 
-##    for edge in edges:
-##        z+=6 #la camera est positionee a z=0, on ne veut pas etre dans le cube
-##        f=SWidth/z #un coefficient de putain de 3d machin la de comment ca ressort et tout putain en 3d stereoscopique on en parle bcp de cette merde
-##        x,y = int(x*f)+SWidth, int(y*f)+SHeight  #donne la position de x et y en pixels
-##
-##        verticesPxPos.append([x,y]) #"position de ces sommets sur l'image 2D"
-##        PILimg.putpixel ((x,y), ( 0, 0, 0 )) #on met tout sur l'image
-
-
-
-
-    draw = ImageDraw.Draw(PILimg)       
-    for A,B in edges :                                             
-        draw.line((verticesPxPos[A][0],verticesPxPos[A][1], verticesPxPos[B][0],verticesPxPos[B][1]), fill=128)
+            #x et y doivent cerrespondre a des pixels du canvas !
+            x = clamp(x,0,WIDTH)
+            y = clamp(y,0,HEIGHT)
+            
+            points.append([x,y]) 
+            #PILimg.putpixel ((x,y), ( 255, 0, 0 )) #on met les sommets
+        draw.line((points[0][0],points[0][1], points[1][0],points[1][1]), fill=128)
     del draw
-    
+
 
     img = ImageTk.PhotoImage(PILimg)
-    canvas.create_image(SWidth, SHeight, image=img) #jsp pq faut utiliser SW et SH
+    oneFrame = canvas.create_image(SWidth, SHeight, image=img) #jsp pq faut utiliser SW et SH
 
-    #root.canvas.update_idletasks #affiche l'image WTF MAINTENANT IL VEUT PLUS UTILLISER CA KWELRFPHOWTGIUIHOGREUW
-    root.update() #end
-
+    root.update()
+    canvas.delete(oneFrame)
+    t1 = time.time()
+    print(1/(t1-t0),"fps")
 root.mainloop() #end
