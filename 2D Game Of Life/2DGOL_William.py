484065dc68667aa6f875557e8cdf11f6dd9cc3f8
William Maillet <mailletwil@gmail.com>
2018-03-20 18:16:37 +0100

    Update 2DGOL_William.py

--- 2D Game Of Life/2DGOL_William.py
+++ 2D Game Of Life/2DGOL_William.py
@@ -16,6 +16,7 @@ from tkinter import *
 from random import randint
 
 
+
 #Création et remplissage aléatoire du tableau
 board = [[randint(0, 1) for i in range(20)] for j in range(20)]
 
@@ -42,6 +43,7 @@ def display():
     window.update()
 
 
+
 def neighborsFinding():
     "Chercher voisins + calculer couleur nouvelle case"
     #Parcours du tableau de long en large
@@ -55,19 +57,8 @@ def neighborsFinding():
                 for j in range(-1, 2): #Test voisins dans cases dans les lignes
 
 
-                    neighbors +=
-                    board[ (x+i+len(board)) % len(board) ]
-                         [ (y+j+len(board[0])) % len(board[0]) ]
+                    neighbors+=board[(x+i+len(board))%len(board)][(y+j+len(board[0]))%len(board[0])]
 
-'''
-Explication : On voudrait voisins += board[x+i][y+j], afin d'avoir les
-coordonnées de la case du voisin et d'ajouter sa valeur à la variable
-"neighbors", mais un soucis se pose : quand on est en extrémité de tableau, on
-a des erreurs du type 'index out of range' car on sort du tableau, et la seule
-manière de régler ceci est en ajoutant le nombre de colonne/de ligne et de
-prendre le modulo de ce qu'il reste pour revenir en début de ligne et être sûr
-de ne pas avoir d'erreur parce qu'on est sorti du tableau.
-'''
 
             #Retrait de la cellule étudiée (pas voisine)
             neighbors -= board[x][y]
@@ -76,30 +67,25 @@ de ne pas avoir d'erreur parce qu'on est sorti du tableau.
             if board[x][y]==0 and neighbors==3:
                 board[x][y] = 1
 
-            elif (board[x][y]==1 and neighbors==3) or
-            (board[x][y]==1 and neighbors==2):
+            elif (board[x][y]==1 and neighbors==3) or (board[x][y]==1 and neighbors==2):
                 board[x][y] = 1
 
             else:
                 board[x][y] = 0
 
-'''
-Rappel des règles (d'après Wikipédia, mais cela pourra être changé par la suite)
-: Une cellule morte possédant exactement trois voisines doit naître, et une
-cellule vivante possédant deux ou trois voisine doit rester vivante. Les autres
-cellules meurent.
-'''
 
     #Après le calcul de toutes les nouvelles couleurs, affichage.
     display()
+
 
 
 def loop():
     "Lance les étapes infinies"
     global a
     a = 1
-    while a==1:
+    while a<100:
         neighborsFinding()
+        a+=1
 
 def stop():
     "Stop les étapes infinies"
@@ -118,6 +104,7 @@ if __name__ == "__main__":
     canvas = Canvas(window, width=c*20, height=c*20, bg="white")
     canvas.grid(column=1, row=1, padx=10, pady=10, rowspan=10)
     display()
+
     #Bouton pour faire une étape
     stepButton = Button(window, text="Étape suivante", command=neighborsFinding)
     stepButton.grid(column=2, row=1, padx=10, pady=10, columnspan=2)
@@ -129,7 +116,5 @@ if __name__ == "__main__":
     stopButton = Button(window, text="Stop", command=stop)
     stopButton.grid(column=3, row=2, padx=10, pady=10)
 
-    #Activation du gestionnaire d'évènement de ma fenêtre
+    #Activation du gestionnaire d'évènement de la fenêtre
     window.mainloop()
-
-    loop()
