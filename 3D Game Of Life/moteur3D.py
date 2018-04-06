60b73e811ba940f5872c02906e820603f41015d0
Woimbée Arthur <arthur.woimbee@gmail.com>
2018-04-06 08:59:12 +0200

    moteur3D : suppression code obselete, refactorisation Ajout de commentaires, le travail sur le spaghetti code semble déjà quasi terminé

--- 3D Game Of Life/moteur3D.py
+++ 3D Game Of Life/moteur3D.py
@@ -1,7 +1,6 @@
 ﻿import tkinter
 from PIL import ImageTk, Image
 import math, random
-import os
 import _thread
 import time
 
@@ -20,20 +19,19 @@ class Camera:
 def keydown(event):
     if event.keysym not in pressedkeys:
         pressedkeys.append(event.keysym)
-
 def keyup(event):
     if event.keysym in pressedkeys:
         pressedkeys.pop(pressedkeys.index(event.keysym))
 
 def movement():
-    """Calcule le déplacement et l'ordre d'affichage des objets en fonction de l'input."""
-
+    """Calcule le déplacement de la camera"""
     sensMouv = 1/10 #sensibilite des mouvements
     sensRot = 1/30 #sensibilite de la rotation
     
     while(1):
         time.sleep(0.01)
         for key in pressedkeys:
+            #déplacement
             x,y = math.sin(cam.rot[1])*sensMouv, math.cos(cam.rot[1])*sensMouv
             if key == 'd':
                 cam.pos[0]+=y
@@ -53,13 +51,9 @@ def movement():
             elif key == 'e':
                 cam.pos[1]-=sensMouv
 
-            #exit
-            elif key == 'Escape':
-                _thread.interrupt_main() #c'est pas fou comme solution
-
             #rotation
-            # axe X     axe Y
-            # rot[1]    rot[0]
+            # axe X  |  axe Y
+            # rot[1] |  rot[0]
             elif key == 'Left':
                 cam.rot[1]-=sensRot
             elif key == 'Right':
@@ -69,15 +63,20 @@ def movement():
             elif key == 'Down':
                 cam.rot[0]+=sensRot
 
+            elif key == 'Escape':
+                _thread.interrupt_main() #exit
 
-def rotate2D(pos, rotation):
-    #pos[0] represente x ou y en fonction des cas, pos[1]=z
+
+def rotate2D(vertex, rotation):
+    """Rotation en 2 dimensions de l'axe partant de l'origine vers le point "vertex" """
+    #https://www.siggraph.org/education/materials/HyperGraph/modeling/mod_tran/2drota.htm
     sin=math.sin(rotation)
     cos=math.cos(rotation)
-    return pos[0]*cos-pos[1]*sin, pos[1]*cos+pos[0]*sin
+    return vertex[0]*cos-vertex[1]*sin, vertex[1]*cos+vertex[0]*sin
 
 
 def gameOfLife(objects, objNb):
+    """ PLACEHOLDER """
     while(1):
         time.sleep(0.1)
 
@@ -101,36 +100,20 @@ def gameOfLife(objects, objNb):
 
                     objects[x][y][z].life = (life_sum*2)%9
 
-class Object:
-    ''' Classe représentant tous les objets '''
-    depth=0
-    show=False
 
-    def __init__(self, position=(0,0,0), life=0):
-        x,y,z = position
-        self.pos = position
-        #on calcule les coordonnees de chaque arrete du cube en fonction de ses dimensions et sa position dans l'espace
-        # X,Y,Z = un point de l'objet
-        self.vertices = [(x+X, y+Y, z+Z) for X,Y,Z in self.vertices]
-        #jsp pas pq faut diviser par 2 mais ça fonctionne mieux <- ça change juste les dimensions des objets
+
+class Cube:
+    #sommets du cube quand il est positionné sur l'origine des reperes x,y,z puis ses faces
+    vertices = (-1,1,-1),(1,1,-1),(1,-1,-1),(-1,-1,-1), (-1,1,1),(1,1,1),(1,-1,1),(-1,-1,1)
+    faces = (4,5,6,7),(0,3,7,4),(1,2,6,5),(3,2,6,7),(0,1,5,4),(0,1,2,3)
+    depth=0
+    
+    def __init__(self, objPos=(0,0,0), life=0):
+        self.pos = objPos
+        #on calcule les coordonnees de chaque point du cube en fonction de sa position à l'origine et de la position de l'objet dans l'espace
+        self.vertices = [(objPos[0]+X, objPos[1]+Y, objPos[2]+Z) for X,Y,Z in self.vertices]
         self.life=life
 
-class Cube(Object):
-    #sommets du cube quand il est positionné sur l'origine des reperes x,y,z puis ses arretes, puis ses faces
-    vertices = (-1,1,-1),(1,1,-1),(1,-1,-1),(-1,-1,-1), (-1,1,1),(1,1,1),(1,-1,1),(-1,-1,1)
-    #edges = (0,1),(1,2),(2,3),(3,0), (0,4), (1,5),(2,6),(3,7), (4,5),(5,6),(6,7),(7,4) #ne sert a rien
-    faces = (4,5,6,7),(0,3,7,4),(1,2,6,5),(3,2,6,7),(0,1,5,4),(0,1,2,3)
-
-    def __init__(self, position=(0,0,0), color="white"):
-        Object.__init__(self, position, color)
-
-class FloorPannel(Object):
-    vertices = (1,1,1),(1,1,-1),(-1,1,-1),(-1,1,1)
-    faces = ((0,1,2,3),(0,1,2,3),(0,1,2,3),(0,1,2,3) ) #Dirty patch, c'est à cause de "face_list.extend(obj_faces[3:])" plus bas
-
-    def __init__(self, position=(0,0,0), color="white"):
-        Object.__init__(self, position, color)
-        
 
 ##Création de la fenetre
 root = tkinter.Tk()
@@ -138,7 +121,7 @@ frame = tkinter.Frame(root, width=WIDTH, height=HEIGHT)
 frame.grid_rowconfigure(0, weight=1)
 frame.grid_columnconfigure(0, weight=1)
 #création du canvas et paramétrage de la récupération de l'entrée utilisateur
-canvas = tkinter.Canvas(frame, width=WIDTH, height=HEIGHT, bg="#ffffff")
+canvas = tkinter.Canvas(frame, width=WIDTH, height=HEIGHT, bg="#343D46")
 canvas.grid(row=0, column=0, sticky=tkinter.N+tkinter.S+tkinter.E+tkinter.W)
 root.bind("<KeyPress>", keydown)
 root.bind("<KeyRelease>", keyup)
@@ -146,9 +129,7 @@ root.bind("<KeyRelease>", keyup)
 frame.pack()
 
 ##Fond d'ecran
-current_file_dir = os.path.dirname(__file__)
-img_path = os.path.join(current_file_dir, "./background.jpg")
-img = Image.open(img_path)
+img = Image.open("./background.jpg")
 x, y = int(HEIGHT*(16/9)), HEIGHT
 img = img.resize((x,y))
 tkimg = ImageTk.PhotoImage(img)
@@ -161,8 +142,8 @@ cam = Camera((0,0,-6))
 #Création des objets à afficher
 objects_3D = [] 
 #objects.extend([ FloorPannel((x,0,z),"white") for z in range(-5,5) for x in range(-5,5) ])
-objects_3D.extend([ [[Cube((x,y,z), random.choice([0,1,2,3,4])) for x in range(0,-40,-4)] for y in range(0,40,4)] for z in range(0,40,4)  ]) #liste 3D pour le jeu de la vie
-objects = [obj for dimension1 in objects_3D for dimension2 in dimension1 for obj in dimension2] # liste 2D pour le rendu 3d
+objects_3D.extend([ [[Cube((x,y,z), random.choice([0,1,2,3,4])) for x in range(0,-40,-4)] for y in range(0,4,4)] for z in range(0,40,4)  ]) #liste 3D pour le jeu de la vie
+objects = [obj for dimension1 in objects_3D for dimension2 in dimension1 for obj in dimension2] # liste 1D pour le rendu 3d
 
 
 i,frameRate=0,0
@@ -175,44 +156,46 @@ while True:
 
     #on calcule comment dessiner chaque cube
     for obj in objects :
-        if obj.life>0 :
-            obj_faces=[]
-            for face in obj.faces:
-                
-                depth = 0
-                face_points = [] #contient 4 sommets a connecter -> (x,y),(x,y),(x,y),(x,y)
-                for x,y,z in (obj.vertices[face[0]], obj.vertices[face[1]], obj.vertices[face[2]], obj.vertices[face[3]]):
-                    #le monde bouge par rapport a la camera
-                    x-=cam.pos[0]
-                    y-=cam.pos[1]
-                    z-=cam.pos[2]
+        obj_faces=[]
+        for face in obj.faces:
+            depth = 0
+            face_points = [] #contient 4 sommets a connecter -> (x,y),(x,y),(x,y),(x,y)
+            for x,y,z in (obj.vertices[face[0]], obj.vertices[face[1]], obj.vertices[face[2]], obj.vertices[face[3]]):
+		#le monde bouge par rapport a la camera
+                x-=cam.pos[0]
+                y-=cam.pos[1]
+                z-=cam.pos[2]
 
-                    x,z = rotate2D((x,z),cam.rot[1]) #x et z modifies par la rotation autour de y
-                    y,z = rotate2D((y,z),cam.rot[0]) #y et z modifies par la rotation autour de x
+                x,z = rotate2D((x,z),cam.rot[1]) #x et z modifies par la rotation autour de y
+                y,z = rotate2D((y,z),cam.rot[0]) #y et z modifies par la rotation autour de x
 
-                    if z<=0:
-                        face_points = None
-                        break
-
-                    f=SWidth/z #coefficient de stereoscopie
-                    X,Y = int(x*f)+SWidth, int(y*f)+SHeight #position en pixels des sommets sur l'image 2D ; +SWidth et SHeight car le repere xyz est placé au milieu de l'ecran
-
-                    if not 0<X<WIDTH or not 0<Y<HEIGHT : #on affiche pas ce qui est hors champ
-                        face_points = None
-                        break
-
-                    face_points.append((X, Y)) #position en pixels des sommets sur l'image 2D
-                    depth += (x**2)+(y**2)+(z**2) #se calcule avec *petit* x,y,z car ils sont position en 3d là où Y,X sont en 2D
-
-                if face_points is None:
+                if z<=0:
+                    #on affiche pas ce qui est hors champ
+                    face_points = None
                     break
-                face_points.extend( (obj.life, depth) ) #face_points contient les coordonnees des points de la face, mais aussi la couleur et profondeur de la face
-                obj_faces.append(face_points)
 
-            obj_faces.sort(key=lambda x: x[-1], reverse=True)
-            face_list.extend(obj_faces[-3:]) #on ne dessine que les 3 faces maximum visibles simultanément
+                f=SWidth/z #coefficient de stereoscopie
+                X,Y = int(x*f)+SWidth, int(y*f)+SHeight #position en pixels des sommets sur l'image 2D ; +SWidth et +SHeight car le repere xyz est placé au milieu de l'ecran
 
-    face_list.sort(key=lambda x: x[-1], reverse=True) 
+                if not 0<X<WIDTH or not 0<Y<HEIGHT :
+                    #on affiche pas ce qui est hors champ
+                    face_points = None
+                    break
+
+                face_points.append((X, Y)) #position en pixels des sommets sur l'image 2D
+                depth += (x**2)+(y**2)+(z**2) #se calcule avec *petit* x,y,z car ils sont position en 3d là où Y,X sont en 2D
+
+            if face_points is None:
+                #on arrete decalculer les faces de l'objet
+                break
+            face_points.extend( (obj.life, depth) ) #face_points contient les coordonnees des points de la face, mais aussi la couleur et profondeur de la face
+            obj_faces.append(face_points)
+
+        obj_faces.sort(key=lambda x: x[-1], reverse=True)
+        face_list.extend(obj_faces[-3:]) #[-3:] car on ne dessine que les 3 faces maximum visibles simultanément
+
+    face_list.sort(key=lambda x: x[-1], reverse=True)
+    #On dessine les faces :
     for face in face_list:
         canvas.create_polygon(face[:-2], fill=colors[face[-2]], outline="black", tag="face")
 
